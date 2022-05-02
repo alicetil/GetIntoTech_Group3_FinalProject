d29458b98b2659f12561025493a697c8ea96857b
Olivia Cottrell <olivia.cottrell23@gmail.com>
2022-05-02 12:38:34 +0100

    First card layout on places base tempalte

--- website/dbScript.py
+++ website/dbScript.py
@@ -2,14 +2,17 @@ import sqlalchemy
 print(sqlalchemy.__version__)
 from sqlalchemy import text
 
+#from sqlalchemy.orm import sessionmaker
 
 import pymysql
 print(pymysql.__version__)
 
 
 from sqlalchemy import create_engine
-engine = create_engine("mysql+pymysql://root:@localhost/visiting_sky", echo=False, future=True)
+engine = create_engine('mysql+pymysql://root:@localhost/visiting_sky_db', echo=False, future=True)
+#Session = sessionmaker(bind=engine)
 
+#session = Session()
 
 with engine.connect() as conn:
     result = conn.execute(text("select * from restaurants"))
